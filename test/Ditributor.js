97ea8e42b1d8931e24060a01cda691437094c534
Dominik Kienzler <dominik.kienzler@vonaffenfels.de>
2016-10-26 12:07:46 +0200

    basic test setup

--- test/Ditributor.js
+++ test/Ditributor.js
@@ -1,7 +1,13 @@
 "use strict";
 
 var Distributor = require("../index.js").Distributor;
-var assert = require("assert");
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+var assert = chai.assert;
+var expect = chai.expect;
+
+setupFTP();
 
 describe('Distributor', function () {
 
@@ -10,18 +16,34 @@ describe('Distributor', function () {
     });
 
     it('should detect non existent files', function () {
-        var distributor = new Distributor();
-        return distributor.distributeFile("package.json").then(() => {
-            console.log("what ?");
-            // should never be called..
-            throw new Error("The file actually exists... stop messing with the tests ;)");
-        }, (err) => {
-            console.log(err);
-            return Promise.resolve();
-        });
+        return expect(new Distributor().distributeFile("XXXXXXXXXXX")).to.eventually.be.rejected;
     });
 
     describe('FTP', function () {
+        it('should transfer', function () {
+            return expect(new Distributor({
+                root: process.cwd(),
+                servers: [
+                    {
+                        type: "ftp",
+                        connection: {
+                            host: "127.0.0.1",
+                            port: 14356
+                        },
+                        root: process.cwd()
+                    }
+                ]
+            }).distributeFile("/test/data/file1.txt")).to.eventually.be.resolved;
+        });
+    });
+
+    describe('SFTP', function () {
+        it('should transfer', function () {
+
+        });
+    });
+
+    describe('SFTP + FTP', function () {
         it('should transfer', function () {
 
         });
@@ -29,3 +51,18 @@ describe('Distributor', function () {
 
 
 });
+
+
+function setupFTP() {
+    var ftpd = require('ftpd');
+    var ftpserver = new ftpd.FtpServer("127.0.0.1", {
+        getInitialCwd: function () {
+            return "/";
+        },
+        getRoot: function () {
+            return process.cwd() + "/test/data_target/";
+        },
+        allowUnauthorizedTls: true
+    });
+    ftpserver.listen(14356);
+}
